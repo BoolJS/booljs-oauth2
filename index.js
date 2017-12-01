8374eb5c359eee8f7c0240c6df92cc19a71f035c
Pablo Andrés Dorado Suárez <im@pandres95.me>
2017-11-30 20:27:03 -0500

    Update code to match latest API and specs of Node LTS

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 'use strict';
 
-const API = require('booljs-api');
+const { Middleware } = require('booljs.api');
 const lib = require('./lib/methods');
 
-module.exports = class BoolJSOAuth2 extends API.Middleware {
-    constructor(){
-        super('booljs-oauth2');
-    }
+module.exports = class BoolJSOAuth2 extends Middleware {
+    constructor () { super('booljs.oauth2'); }
 
-    action(_instance) {
-        lib(_instance);
+    action (instance) {
+        lib(instance);
         return (req, res, next) => next();
     }
 };
