666d996739bf171b035955aa29ac06fc90e3668a
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2018-10-24 01:40:25 -0500

    Test: Conform to BoolJS v0.8.x

--- test/_.js
+++ test/_.js
@@ -2,18 +2,28 @@
 
 const Bool = require('booljs');
 
-let api = new Bool('com.example.api', [
-    'booljs.passport',
-    'passport-http-bearer', 'passport-http', 'passport-oauth2-client-password',
-    require.resolve('..')
-]);
-
-api
-    .setBase('example')
-    .run();
-
 const chai = require('chai');
 chai.use(require('chai-as-promised'));
 
 global.expect = chai.expect;
 global.Agent = require('supertest');
+
+async function main () {
+    try {
+        let API = new Bool('com.example.api', [
+            '@booljs/express', '@booljs/passport',
+            'passport-http-bearer', 'passport-http',
+            'passport-oauth2-client-password',
+            require.resolve('..')
+        ]);
+
+        return API
+            .setBase('example')
+            .setServerDrivers('booljs.express')
+            .run();
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = main();
